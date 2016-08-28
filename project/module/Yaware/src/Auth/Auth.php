bf364d405f826517bf4f268bb630b05835a19b67
Martsynyuk <terr83@gmail.com>
2016-08-28 12:27:10 +0300

    1

--- project/module/Yaware/src/Auth/Auth.php
+++ project/module/Yaware/src/Auth/Auth.php
@@ -5,9 +5,10 @@ session_start();
 
 class Auth 
 {
-	public function auth($id)
+	public function auth($id, $status)
 	{
 		$_SESSION['id'] = $id;
+		$_SESSION['status'] = $status;
 	}
 	
 	public function getUserId()
@@ -16,5 +17,19 @@ class Auth
 			return $_SESSION['id'];
 		}
 		return false;
+	}
+	
+	public function getUserStatus()
+	{
+		if(isset($_SESSION['status'])) {
+			return $_SESSION['status'];
+		}
+		return 'guest';
+	}
+	
+	public function logout()
+	{
+		unset($_SESSION['status']);
+		unset($_SESSION['id']);
 	}
 }
--- project/module/Yaware/src/Controller/UserController.php
+++ project/module/Yaware/src/Controller/UserController.php
@@ -1,6 +1,8 @@
 <?php
 namespace Yaware\Controller;
 
+use Zend\Permissions\Acl\Acl;
+use Zend\Permissions\Acl\Role\GenericRole as Role;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 use Yaware\Model\UserTable;
@@ -21,31 +23,14 @@ class UserController extends AbstractActionController
 	
 	public function indexAction()
 	{
-		$form = new UserForm();
-		$form->get('submit')->setValue('New User');
-		
-		$request = $this->getRequest();
-		
-		if (! $request->isPost()) {
-			return ['form' => $form];
-		}
-		
-		$user = new User();
-		$form->setInputFilter($user->getInputFilter());
-		$form->setData($request->getPost());
-		
-		if (! $form->isValid()) {
-			return ['form' => $form];
-		}
-
-		$user->exchangeArray($form->getData());
-		$this->table->saveUser($user);
-		return $this->redirect()->toUrl('/');
-		
+		$this->autorization('index');
+		return new ViewModel();
 	}
 	
 	public function loginAction()
 	{
+		$this->autorization('login');
+		
 		$form = new LoginForm();
 		$form->get('submit')->setValue('Login');
 		
@@ -67,7 +52,9 @@ class UserController extends AbstractActionController
 		
 		if($this->table->getUser($user)) {
 			$auth = new Auth();
-			$auth->auth($this->table->getUser($user)->id);
+			$auth->auth($this->table->getUser($user)->id,
+						$this->table->getUser($user)->status
+					);
 			return $this->redirect()->toUrl('/');
 		}
 		return ['form' => $form];
@@ -75,6 +62,8 @@ class UserController extends AbstractActionController
 	
 	public function registrationAction()
 	{
+		$this->autorization('registration');
+		
 		$form = new RegistrationForm();
 		$form->get('submit')->setValue('registration');
 		
@@ -97,18 +86,86 @@ class UserController extends AbstractActionController
 		return $this->redirect()->toUrl('/user/login');
 	}
 	
+	public function createUserAction()
+	{
+		$this->autorization('createUser');
+		
+		$form = new UserForm();
+		$form->get('submit')->setValue('New User');
+		
+		$request = $this->getRequest();
+		
+		if (! $request->isPost()) {
+			return ['form' => $form];
+		}
+		
+		$user = new User();
+		$form->setInputFilter($user->getInputFilter());
+		$form->setData($request->getPost());
+		
+		if (! $form->isValid()) {
+			return ['form' => $form];
+		}
+
+		$user->exchangeArray($form->getData());
+		$this->table->saveUser($user);
+		return $this->redirect()->toUrl('/');
+	}
+	
 	public function dashboardAction()
 	{
+		$this->autorization('dashboard');
 		return new ViewModel();
 	}
 	
 	public function reportsAction()
 	{
+		$this->autorization('reports');
 		return new ViewModel();
 	}
 	
 	public function configurationAction()
 	{
+		$this->autorization('configuration');
 		return new ViewModel();
+	}
+	
+	public function autorizationRules()
+	{	
+		$acl = new Acl();
+		
+		$acl->addRole(new Role('guest'));
+		$acl->addRole(new Role('owner'));
+		$acl->addRole(new Role('employee'));
+		$acl->addRole(new Role('admin'));
+		
+		$acl->allow('guest', null, ['login', 'registration']);
+		$acl->allow('owner', null, ['index', 'createUser', 'dashboard', 'configuration', 'reports']);
+		$acl->allow('employee', null, ['index', 'dashboard', 'reports']);
+		$acl->allow('admin', null, ['index', 'dashboard', 'configuration']);
+
+		return $acl;
+	}
+	
+	public function autorization($action)
+	{
+		$acl = $this->autorizationRules();
+		
+		$status = new Auth();
+		$status = $status->getUserStatus();
+		
+		if(!$acl->isAllowed($status, null, $action)) {
+			if($status == 'guest') {
+				return $this->redirect()->toUrl('/user/login'); 
+			}
+			return $this->redirect()->toUrl('/');
+		}
+	}
+	
+	public function logoutAction()
+	{
+		$auth = new Auth();
+		$auth->logout();
+		return $this->redirect()->toUrl('/user/login');
 	}
 }
